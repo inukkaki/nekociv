75e745cf7100696a2c255fb1c10b9dd0b102f2f6
inukkaki <kgn.gkrk.rstb@gmail.com>
2025-11-16 23:19:28 +0900

    Modify src.tests.popl

--- tests/popl.py
+++ tests/popl.py
@@ -5,16 +5,30 @@ import matplotlib.pyplot as plt
 from src.civ.group import Group
 from src.field.cell import Cell
 
-N_TURN = 3000
+N_TURN = 1000
 
 
 if __name__ == "__main__":
     # Players
-    cell = Cell(0, 0)
-    cell.stpn = 0.0007
-    cell.surface = Cell.SURFACE_LAND
+    c1 = Cell(0, 0)
+    c1.stpn = 0.0007
+    c1.surface = Cell.SURFACE_LAND
 
-    group = Group(10, 20, cell)
+    c2 = Cell(0, 1)
+    c2.stpn = 0.0005
+    c2.surface = Cell.SURFACE_LAND
+
+    c3 = Cell(1, 0)
+    c3.stpn = 0.0001
+    c3.surface = Cell.SURFACE_LAND
+
+    c1.neighborhood = [c2, c3]
+    c2.neighborhood = [c3, c1]
+    c3.neighborhood = [c1, c2]
+
+    Group(10, 20, [0.5, 0.5, 0.5], c1)
+    Group(10, 20, [0.5, 0.5, 0.5], c2)
+    Group(10, 20, [0.5, 0.5, 0.5], c3)
 
     # Simulation
     sim_seed = 1
@@ -26,23 +40,30 @@ if __name__ == "__main__":
     diffs = []
 
     for i in range(N_TURN):
-        group.update()
+        if c1.group != None and c1.group.alive:
+            c1.group.update()
+        if c2.group != None and c2.group.alive:
+            c2.group.update()
+        if c3.group != None and c3.group.alive:
+            c3.group.update()
 
         xs.append(i)
-        popls.append(group.popl)
-        foods.append(group.food)
-        diffs.append(group.diff)
-
-        if not group.alive:
-            break
+        if c1.group != None:
+            popls.append(c1.group.popl)
+            foods.append(c1.group.food)
+            diffs.append(c1.group.diff)
+        else:
+            popls.append(0)
+            foods.append(0)
+            diffs.append(0.0)
 
     # Graph
     fig, ax = plt.subplots(2, 1)
     ax_pf = ax[0]
     ax_d = ax[1]
 
-    ax_pf.plot(xs, popls, label="Popl.")
-    ax_pf.plot(xs, foods, label="Food")
+    ax_pf.plot(xs, popls, label="Popl.", alpha=0.5)
+    ax_pf.plot(xs, foods, label="Food", alpha=0.5)
     ax_pf.legend()
     ax_pf.set_ylabel("Value")
 
